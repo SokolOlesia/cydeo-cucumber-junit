0726c96eded89b0b0718b1e790b9f44ff62533b3
Olesia Sokol <lesy0101@me.com>
2022-03-18 16:37:49 -0400

    day 16_GoogleStepDefinitions

--- src/test/java/com/cydeo/step_definitions/DataTable_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/DataTable_StepDefinitions.java
@@ -7,14 +7,7 @@ import java.util.List;
 public class DataTable_StepDefinitions {
     @Then("user should see fruits I like")
     public void user_should_see_fruits_i_like(List<String> fruits) {
-        // Write code here that turns the phrase above into concrete actions
-        // For automatic transformation, change DataTable to one of
-        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-        // Double, Byte, Short, Long, BigInteger or BigDecimal.
-        //
-        // For other transformations you can register a DataTableType.
-        throw new io.cucumber.java.PendingException();
+        System.out.println(fruits);
     }
 
 }
