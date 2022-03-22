e26f2fee200f8347ccebb224e4c545edc0e42852
Olesia Sokol <lesy0101@me.com>
2022-03-22 11:33:03 -0400

    day 18_webTableOrder

--- src/test/java/com/cydeo/pages/OrderPage.java
+++ src/test/java/com/cydeo/pages/OrderPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class OrderPage {
     public OrderPage() {
         PageFactory.initElements(Driver.getDriver(), this);
@@ -29,4 +31,7 @@ public class OrderPage {
 
     @FindBy(name = "zip")
     public WebElement inputZip;
+
+    @FindBy(name = "card")
+    public List<WebElement> cardType;
 }
